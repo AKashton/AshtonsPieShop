7479d615a8934225b8ad9aaa84bab8301348a50b
AKashton <ashtonrcurry@hotmail.com>
2024-06-14 10:28:49 -0800

    Added Shopping Cart model, Interface, and database table.

---
+++ AshtonsPieShop/Models/ShoppingCart.cs
@@ -0,0 +1,108 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace AshtonsPieShop.Models
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        private readonly AshtonsPieShopDbContext _ashtonsPieShopDbContext;
+
+        public string? ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+
+        private ShoppingCart(AshtonsPieShopDbContext ashtonsPieShopDbContext)
+        {
+            _ashtonsPieShopDbContext = ashtonsPieShopDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            AshtonsPieShopDbContext context = services.GetService<AshtonsPieShopDbContext>() ?? throw new Exception("Error initializing");
+
+            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session?.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _ashtonsPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+                _ashtonsPieShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _ashtonsPieShopDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _ashtonsPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _ashtonsPieShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _ashtonsPieShopDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??=
+                       _ashtonsPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                           .Include(s => s.Pie)
+                           .ToList();
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _ashtonsPieShopDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+            _ashtonsPieShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+            _ashtonsPieShopDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _ashtonsPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+    }
+}

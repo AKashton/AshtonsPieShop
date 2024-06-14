aaeb5d134b97fe172e295d82bff3a2d134a28439
arcurry <Ashton.Curry@alaska.gov>
2024-06-14 09:51:21 -0800

    added routing to details, a home page with pies of the week, and tag helpers.

--- AshtonsPieShop/Controllers/PieController.cs
+++ AshtonsPieShop/Controllers/PieController.cs
@@ -20,5 +20,16 @@ namespace AshtonsPieShop.Controllers
             PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if(pie == null) 
+                return NotFound();
+
+            return View(pie);
+        }
+
     }
 }

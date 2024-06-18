899e69fad73eb3fa4b087733ebdeab40bc56d642
AKashton <ashtonrcurry@hotmail.com>
2024-06-17 16:08:40 -0800

    implemented the website to the Azure Cloud.

--- AshtonsPieShop/Migrations/20240617234727_fixBlueBerryPie.Designer.cs
+++ AshtonsPieShop/Migrations/20240617234727_fixBlueBerryPie.Designer.cs
@@ -4,6 +4,7 @@ using AshtonsPieShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AshtonsPieShop.Migrations
 {
     [DbContext(typeof(AshtonsPieShopDbContext))]
-    partial class AshtonsPieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240617234727_fixBlueBerryPie")]
+    partial class fixBlueBerryPie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
